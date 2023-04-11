f1e515418f3f18316ce9a4bda0e5b5841be423a0
Axel Wulff Sæther <axel.sather@soprasteria.com>
2023-04-11 14:57:26 +0200

    Etter Section 32: Spring WebClient

---
+++ spring-6-webclient/src/main/java/guru/springframework/spring6webclient/client/BeerClient.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring6webclient.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import guru.springframework.spring6webclient.model.BeerDTO;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+public interface BeerClient {
+
+    Flux<String> listBeer();
+
+    Flux<Map> listBeerMap();
+
+    Flux<JsonNode> listBeersJsonNode();
+
+    Flux<BeerDTO> listBeerDtos();
+
+    Mono<BeerDTO> getBeerById(String id);
+
+    Flux<BeerDTO> getBeerByBeerStyle(String beerStyle);
+
+    Mono<BeerDTO> createBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> updateBeer(BeerDTO dto);
+
+    Mono<BeerDTO> patchBeer(BeerDTO dto);
+
+    Mono<Void> deleteBeer(BeerDTO dto);}
